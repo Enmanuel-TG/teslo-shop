ff203b078da3c28326efd0a121d410b13801a9a6
Enmanuel-TG <enmanueltg7@gmail.com>
2025-06-26 08:57:00 -0400

    add a hover display in product images

--- src/components/products/ProductGrid/ProductGridItem.tsx
+++ src/components/products/ProductGrid/ProductGridItem.tsx
@@ -1,27 +1,35 @@
-import Image from "next/image";
-import { Product } from "../../interfaces"
-import Link from "next/link";
+"use client";
 
-interface Props{
-    product: Product;
+import Image from "next/image";
+import { Product } from "../../interfaces";
+import Link from "next/link";
+import { useState } from "react";
+
+interface Props {
+  product: Product;
 }
 
-export const ProductGridItem = ({product}:Props) => {
+export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
   return (
     <div className="rounded-md overflow-hidden float-in">
-          <Link href={`/product/${product.slug}`}>
-      <Image
-        src={`/products/${product.images[0]}`}
-        alt={product.title}
-        className="w-full object-contain"
-        width={500}
-        height={500}
-      />
-          </Link>
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={`/products/${displayImage}`}
+          alt={product.title}
+          className="w-full object-contain rounded"
+          width={500}
+          height={500}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
+        />
+      </Link>
       <div className="p-4 flex flex-col ">
-        <Link className="hover:text-blue-600" href={`/product/${product.slug}`}>{product.title}</Link>
+        <Link className="hover:text-blue-600" href={`/product/${product.slug}`}>
+          {product.title}
+        </Link>
         <span className="font-bold">{product.price}</span>
       </div>
     </div>
   );
-}
+};
