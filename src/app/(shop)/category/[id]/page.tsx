fa8e0e7f6895706c43fb264714837b0c7d45ee76
Enmanuel-TG <enmanueltg7@gmail.com>
2025-06-27 20:48:27 -0400

    add a product page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -2,19 +2,21 @@ import { notFound } from "next/navigation";
 import { ProductGrid, Title } from "@/src/components";
 import { initialData } from "@/src/seed/seed";
 import { Category } from "@/src/components/interfaces";
+import { BiCategory } from "react-icons/bi";
 
 interface Props {
   params: {
     id: Category;
   };
 }
+const category: Category[] = ["kid", "men", "women"];
 
 export default async function ({ params }: Props) {
   const { id } = await (params as unknown as Promise<{ id: string }>);
   const seedProduct = initialData.products;
   const products = seedProduct.filter((product) => product.gender === id);
 
-  if (!id || !products) {
+  if (!category.includes(id as Category)) {
     notFound();
   }
 
--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,35 @@
-export default function () {
+import { initialData } from "@/src/seed/seed";
+import notFound from "../../category/not-found";
+import { geistSans } from "@/src/config/fonts";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+const products = initialData.products;
+
+export default function ({ params }: Props) {
+  const { slug } = params;
+  const product = products.find(product => product.slug === slug);
+  if (!product) {
+    return notFound();
+   }
   return (
-      <div>
-          Product for Page
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      <div className="col-span-1 md:col-span-2">hi</div>
+      <div className="col-span-1 px-5">
+        <h1 className={`${geistSans.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-log mb-5">{product.price}</p>
+
+
+        <button className="btn-primary my-5">add to cart</button>
+        <h3 className="font-bold text-sm">description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
-  )
+  );
 }
